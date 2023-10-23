006c6553ce6a46dfa7f0daa7a9b05ed36f5f4b94
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-10-23 09:42:07 +0000

    some changes to the database

--- posts/models.py
+++ posts/models.py
@@ -4,6 +4,6 @@ from datetime import datetime
 # Create your models here.
 
 class Posts (models.Model):
-    heading = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     body = models.CharField(max_length=1000000)
-    date = models.DateTimeField(default=datetime.now, blank=True)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
