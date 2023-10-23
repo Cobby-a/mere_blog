3da06a9692974f796cd260d7f523b5f6a5ebeae1
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-10-23 09:46:56 +0000

    added our models to the views file

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import Posts
 # Create your views here.
 
 def index(request):
-    return(render(request, 'index.html'))
+    posts = Posts.objects.all()
+    return(render(request, 'index.html', {'posts':posts}))
